e8e3c599b43051a90a85b4fcf4d36209ba84ab8e
Vadim Rufov <vadim.rufov@gs-labs.ru>
2021-08-08 20:24:17 +0300

    Initial Commit

---
+++ H4X0R-News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by Вадим Руфов on 08.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4X0R-News-SwiftUI/H4X0R_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_News_SwiftUIApp.swift
+//  H4X0R-News-SwiftUI
+//
+//  Created by Вадим Руфов on 08.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
